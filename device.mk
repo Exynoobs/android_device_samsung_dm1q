f3c4b66eb29e829b13909ba7591db5eed52ac2fd
Vince Borbély <borbely.vince.11@gmail.com>
2026-07-16 12:48:58 +0200

    dm1q: Add display brightness to displayconfig
    Change-Id: I1af1709cf6c1ac1307b3e8a0169614d5f53c0085

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# Display config (stock DDC renamed to live unique display id)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947059332006275.xml
+
 # FlipFlap
 PRODUCT_PACKAGES += \
     FlipFlap
